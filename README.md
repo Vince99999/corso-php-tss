bd6970acc12394466ff68a98b6c382f617e75f34
name <mail@mail.com>
2023-02-06 12:37:47 +0100

    prima di brasare tutto

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # corso-php-tss
-
-aaaaaaaaaaaaa
--- form_in_php/log-in-response.php
+++ form_in_php/log-in-response.php
@@ -3,13 +3,17 @@
 <?php
 
 echo "get:Array";
-
+//  $_ rappresenta una variabile superglobale in particolare le variabili $_GET e $_POST sono variabili superglobali che contengono un array
 echo"<pre>";
 print_r($_GET);
 echo"</pre>";
 
 echo "post:Array";
-
+// il metodo POST consente di modificare/aggiungere il testo ai parametri passati con esso
 echo"<pre>";
 print_r($_POST);
 echo"</pre>";
+
+echo "la tua mail è <br>";
+
+echo "<strong>".$_POST['email']."</strong>";
