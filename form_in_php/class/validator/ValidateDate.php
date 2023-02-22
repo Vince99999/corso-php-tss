36f398be0856e68dc5e3cbf61f9945930e91b8d7
name <mail@mail.com>
2023-02-22 10:35:46 +0100

    test_validate_date_ok

--- form_in_php/class/validator/ValidateDate.php
+++ form_in_php/class/validator/ValidateDate.php
@@ -1,13 +1,45 @@
 <?php
- class ValidateDate {
+require "./form_in_php/class/validator/Validable.php";
+ class ValidateDate implements Validable{
 
-public function isValid(string $date) : bool{
-    $parametriData=explode("-", $date);
-    $anno=$parametriData[0];
-    $mese=$parametriData[1];
-    $giorno=$parametriData[2];
-    
-  return  checkdate($mese, $giorno, $anno);
+// public function isValid($date) : bool{
+
+//     $parametriData=explode("-", $date);
+//     if (isset($parametriData)){
+//     $anno=$parametriData[0];
+//     $mese=$parametriData[1];
+//     $giorno=$parametriData[2];
+//     return  checkdate($mese, $giorno, $anno);
+//   }else{
+
+//     return false;
+//     }
+//   }
+// }
+
+public function isValid($value){
+{
+  $sanitize = trim(strip_tags($value));
+  $dt = DateTime :: createFromFormat('d/m/Y', $sanitize);
+echo $value."\n";
+  print_r($dt->format('d/m/Y'));
+  
+
+
+  if($dt && $dt->format('d/m/Y') === $sanitize){
+    return $dt->format('d/m/Y');
+  }else{
+    return false;
+  };
+}
+
+
+
 
 }
+public function message()
+{
+  return 'data non valida';
+}
+
  }
---
+++ form_in_php/test/test_validazione_data.php
@@ -0,0 +1,79 @@
+<?php
+require "./form_in_php/class/validator/ValidateDate.php";
+$testCases = [
+    [
+        'input' => '33/09/1975',
+
+        'expected' => false
+    ],
+    [
+        'input' => ' 03/09/1975',
+
+        'expected' => '03/09/1975'
+    ],
+      [
+        'input' => '03/09/1975 ',
+
+        'expected' => '03/09/1975'
+    ],
+     [
+        'input' => '<br>03/09/1975 ',
+
+         'expected' => '03/09/1975'
+     ],
+    [
+        'input' => ' 03/09/1975</br>',
+
+        'expected' => '03/09/1975'
+    ],
+    [
+        'input' =>  '<br> 03/09/1975</br>' ,
+
+        'expected' => '03/09/1975'
+    ],
+    [
+        'input' => '<br> 03/09/1975 </br> ' ,
+
+        'expected' => '03/09/1975'
+    ],
+    [
+        'input' => ' 01/01/1900' ,
+
+        'expected' => '01/01/1900'
+    ],
+    [
+        'input' => '01/01/<b>1900</b>' ,
+
+        'expected' => '01/01/1900'
+    ],
+  
+    ];
+
+foreach ($testCases as $key => $test){
+  
+    $input = $test['input'];
+    $expected = $test ['expected'];
+
+    $v = new ValidateDate();
+    if($v -> isValid($input) != $expected){
+        echo "\nTest numero $key non superato mi aspettavo";
+        var_dump($expected);
+        echo "\nma ho trovato";
+        var_dump($v -> isValid($input));
+    };
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
